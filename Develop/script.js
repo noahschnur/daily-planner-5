e313e4319c3fec2450bf2c174093a4ce93d97057
Noah <noahschnur@gmail.com>
2021-03-07 12:55:33 -0800

    localstorage functionality, js, html

--- Develop/script.js
+++ Develop/script.js
@@ -1,41 +1,29 @@
 // Add current day to header
 var currentDay = moment().format("MMMM Do YYYY");
-$("#currentDay").text(currentDay);
+    $("#currentDay").text(currentDay);
 
 // Add hour checking to schedule for past/present/future classes
 var hour = moment().hour();
-$("textarea").each(function () {
-    if (this.id < hour) {
-        $(this).addClass("past");
-        $(this).removeClass("present future");
-    } else if (this.id == hour) {
-        $(this).addClass("present");
-        $(this).removeClass("past future");
-    } else if (this.id > hour) {
-        $(this).addClass("future");
-        $(this).removeClass("past present");
-    }
+    $("textarea").each(function () {
+        if (this.id < hour) {
+            $(this).addClass("past");
+            $(this).removeClass("present future");
+        } else if (this.id == hour) {
+            $(this).addClass("present");
+            $(this).removeClass("past future");
+        } else if (this.id > hour) {
+            $(this).addClass("future");
+            $(this).removeClass("past present");
+        }
 });
 
 // Click save for localStorage
-$(".saveBtn").on("click", function() {
-    var textContent = $(this).val(".task-input");
-
-    localStorage.setItem("task" , textContent)
-    console.log("task", textContent)
+$(".saveBtn").on("click", function () {
+    // get nearby values
+    var content = $(this).siblings(".task-input").val();
+    var time = $(this).siblings().attr('id');
+    // save in localStorage
+    localStorage.setItem(time, content);
+ console.log(time, content)
 });
-
-// Return from local storage
-
-localStorage.getItem(textContent);
-
-// reload time without refreshing page
-
-setInterval(function () {
-    $(".task-input").each(function(index, el) {
-      auditTask(el);
-    });
-  }, (1000 * 60) * 5);
-
-//  clear after end of day
 
