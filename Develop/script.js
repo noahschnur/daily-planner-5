a83d99fbaa5b27b7d05fc947991b733e59375aa2
Noah <noahschnur@gmail.com>
2021-03-07 15:51:27 -0800

    localstage set/get and css fix try 1

--- Develop/script.js
+++ Develop/script.js
@@ -17,13 +17,57 @@ var hour = moment().hour();
         }
 });
 
+ // get from local storage
+loadDate = function() {
+    for (let i = 9; i <= 17; i++) {
+    var newData = localStorage.getItem(i);
+    $(`#${i}`).val(newData);
+
+    console.log(localStorage.getItem(i))};
+};
+
 // Click save for localStorage
 $(".saveBtn").on("click", function () {
     // get nearby values
     var content = $(this).siblings(".task-input").val();
     var time = $(this).siblings().attr('id');
+
     // save in localStorage
     localStorage.setItem(time, content);
- console.log(time, content)
+
+    console.log(time, content)
 });
 
+
+loadDate()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// setInterval(function () {
+//     $(".hour .task-input").each(function(el) {
+//       auditTask(el);
+//       localStorage.getItem(content)
+//     });
+//   }, (1000 * 60) * 5);
+
+// clear at end of day
+// hour >6, localStorage.clear()
